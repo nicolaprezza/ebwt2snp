af39b7036ca84199c6dd013b67558f8137150cd5
Nicola <nicola.prezza@gmail.com>
2018-12-07 12:50:42 +0100

    implementing dna_string

--- internal/dna_string.hpp
+++ internal/dna_string.hpp
@@ -4,13 +4,13 @@
  *  Created on: Dec 6, 2018
  *      Author: nico
  *
- *  Optimized string with rank on DNA alphabet: {A,C,G,T,TERM}, where the terminator TERM is defined in include.hpp
+ *  Optimized string with rank on DNA alphabet: {A,C,G,T,TERM,N}, where the terminator TERM is defined in include.hpp
  *
- *  One access or a block of rank for all 4 letters A,C,G,T causes only 1 cache miss in the worst case
+ *  One access or a parallel rank for all 4 letters A,C,G,T causes only 1 cache miss in the worst case
  *
  *  Max string length: 2^48
  *
- *  Supports very efficient (1 cache miss) combined rank for (A,C,G,T), and (1 cache miss) single rank for other characters ($ + N)
+ *  Supports very efficient (1 cache miss) parallel rank for (A,C,G,T), and (1 cache miss) single rank for other characters combined ($ + N)
  *  Note: if both N's and $'s are present, then can only compute combined rank of N + $!
  *
  *
@@ -23,7 +23,7 @@
  *  - in the next 40 bytes = 5 * 64-bit words = 320 bits, 105 nucleotides, 'N', or terminator character,
  *    encoded using 3-bits per letter. These characters are stored in blocks of 21 per 64-bit word, wasting only 1 bit per word.
  *
- *    Size of the string: 4,876190476 bits / base
+ *    Size of the string: < 4,9 bits / base
  *
  */
 
@@ -38,16 +38,55 @@
 
 struct p_rank{
 
-	uint64_t A;
-	uint64_t C;
-	uint64_t G;
-	uint64_t T;
+	uint64_t A = 0;
+	uint64_t C = 0;
+	uint64_t G = 0;
+	uint64_t T = 0;
+
+	p_rank operator+(const p_rank& a) const{
+		return { a.A + A, a.C + C, a.G + G, a.T + T };
+	}
 
 };
 
 class dna_string{
 
 public:
+
+	/*
+	 * constructor from file
+	 */
+	dna_string(string path){
+
+		n = uint64_t(filesize(path));
+
+		n_blocks_512 = n/BLOCK_SIZE + (n%BLOCK_SIZE != 0);
+		n_blocks_64 = n_blocks_512 * 8;
+
+		data = new uint64_t[n_blocks_64];
+
+		for(uint64_t i=0;i<n_blocks_512;++i) data[i] = 0;
+
+		ifstream ifs(path);
+
+		uint8_t i = 0;
+		while(not ifs.eof()){
+
+			uint8_t c;
+			ifs.read((char*)&c, sizeof(uint8_t));
+
+			if(c != 'A' and c != 'C' and c != 'G' and c != 'T' and c != 'N' and c != TERM){
+
+				cout << "Error while loading file " << path << ": forbidden character '" << c << "'" << endl;
+				exit(1);
+
+			}
+
+			set(i++,c);
+
+		}
+
+	}
 
 	/*
 	 * constructor: allocates a string of length size filled with 'A's
@@ -113,11 +152,28 @@ public:
 
 	}
 
+	/*
+	 * return number of occurrences of A,C,T,G in the prefix of length i of the text. At most 1 cache miss!
+	 */
 	p_rank parallel_rank(uint64_t i){
 
 		assert(i<=n);
+		uint64_t bl = i/BLOCK_SIZE;
+		return get_counters(bl) + block_rank(bl,i%BLOCK_SIZE);
 
-		return {};
+	}
+
+	/*
+	 * return number of non-dna symbols in the prefix of length i of the text. At most 1 cache miss!
+	 */
+	uint64_t rank_non_dna(uint64_t i){
+
+		assert(i<=n);
+		auto r = parallel_rank(i);
+
+		assert(r.A + r.C + r.G + r.T <= i);
+
+		return i - (r.A + r.C + r.G + r.T);
 
 	}
 
@@ -129,8 +185,90 @@ public:
 
 private:
 
+	/*
+	 * A 64-bits word represents a block of SUB_BLOCK_SIZE = 21 characters (leftmost bit wasted).
+	 * This function counts in parallel the number of A,C,G,T in the prefix of length i of the block.
+	 *
+	 */
+	inline p_rank word_rank(uint64_t x, uint8_t i){
+
+		if(i==0) return {};
+
+		//First, replace with '111' (unused code) the last SUB_BLOCK_SIZE - i characters
+
+		x |= ( (uint64_t(1) << (3*(SUB_BLOCK_SIZE - i)))-1 );
+
+		//x shifted to the left 1 and 2 positions
+		uint64_t x1 = x>>1;
+		uint64_t x2 = x1>>1;
+
+		uint64_t A = __builtin_popcountll(((~x2) & MASK) & ((~x1) & MASK) & ((~x) & MASK));//000
+		uint64_t C = __builtin_popcountll(((~x2) & MASK) & ((~x1) & MASK) & ((x) & MASK));//001
+		uint64_t G = __builtin_popcountll(((~x2) & MASK) & ((x1) & MASK) & ((~x) & MASK));//010
+		uint64_t T = __builtin_popcountll(((~x2) & MASK) & ((x1) & MASK) & ((x) & MASK));//011
+
+		return {A,C,G,T};
+
+	}
+
+	/*
+	 * Parallel rank of (A,C,T,G) for the length-j prefix of the i-th block of size BLOCK_SIZE of the text
+	 *
+	 */
+	inline p_rank block_rank(uint64_t i, uint8_t j){
+
+		if(j==0) return {};
+
+		uint64_t start = i*WORDS_PER_BLOCK + 3;//first word containing the block's characters
+
+		uint8_t bl = j/SUB_BLOCK_SIZE; // block where position j lies.
+		uint8_t off = j%SUB_BLOCK_SIZE; // prefix length in the bl-th block
+
+		return	word_rank( data[start],   bl == 0 ? off : SUB_BLOCK_SIZE ) +
+				bl < 1 ? p_rank() : word_rank( data[start+1], bl == 1 ? off : SUB_BLOCK_SIZE) +
+				bl < 2 ? p_rank() : word_rank( data[start+2], bl == 2 ? off : SUB_BLOCK_SIZE) +
+				bl < 3 ? p_rank() : word_rank( data[start+3], bl == 3 ? off : SUB_BLOCK_SIZE) +
+				bl < 4 ? p_rank() : word_rank( data[start+4], off);
+
+	}
+
+	/*
+	 * set counters in the i-th block to r
+	 */
+	void set_counters(uint64_t i, p_rank r){
+
+		uint64_t start = i*WORDS_PER_BLOCK;//start of region where to write the three 48-bits numbers
+
+		data[start]   |= (r.A << 16);//first 48 bits of data[start] contain r.A
+
+		data[start]   |= (r.C >> 32);//last 16 bits of data[start] contain first 16 bits of r.C
+		data[start+1] |= ((r.C & 0x00000000FFFFFFFF) << 32);//first 32 bits of data[start+1] contain last 32 bits of r.C
+
+		data[start+1] |= (r.G >> 16) ; //last 32 bits of data[start+1] contain first 32 bits of r.G
+		data[start+2] |= ((r.G & 0x000000000000FFFF) << 48) ; //first 16 bits of data[start+2] contain last 16 bits of r.G
+
+		data[start+2] |= r.T; //last 48 bits of data[start+2] contain the 48 bits of r.G
+
+	}
+
+	/*
+	 * get counters of the i-th block
+	 */
+	inline p_rank get_counters(uint64_t i){
+
+		return {};
+
+	}
+
+	/*
+	 * MASK =
+	 *    0001 0010 0100 1001 0010 0100 1001 0010 0100 1001 0010 0100 1001 0010 0100 1001 =
+	 * 0x 1    2    4    9    2    4    9    2    4    9    2    4    9    2    4    9
+	 */
+	static const uint64_t MASK = 0x1249249249249249;
+
 	//data aligned with blocks of 64 bytes = 512 bits
-	alignas(64) uint64_t * data;
+	alignas(64) uint64_t * data = NULL;
 
 	uint64_t n_blocks_512 = 0;
 	uint64_t n_blocks_64 = 0;
